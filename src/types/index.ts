71528f259f4ee54469e1c5cb31adc196d9d892e7
leonhfr <hello@leonh.fr>
2019-03-23 06:18:18 +0100

    types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// Packages.
+import * as turfHelpers from '@turf/helpers';
+
+// Definition.
+export type Result<T, E> = {
+  readonly ok: T;
+  readonly err: E;
+};
+
+export type DividedArea = {
+  readonly polygons: Array<turfHelpers.Feature<turfHelpers.Polygon>>;
+  readonly points: Array<Array<number>>;
+};
